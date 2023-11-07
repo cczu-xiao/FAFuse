cbad866f74388a7260317b095d7881e336249f8f
cczu <79259585+cczu-xiao@users.noreply.github.com>
2023-11-07 13:27:30 +0800

    Update DeiT.py

--- lib/DeiT.py
+++ lib/DeiT.py
@@ -41,18 +41,18 @@ class DeiT(VisionTransformer):
 def deit_small_patch16_224(pretrained=False, **kwargs):
     model = DeiT(
         patch_size=16, embed_dim=768, depth=8, num_heads=8, mlp_ratio=4, qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6),**kwargs)   # img_size=384
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),img_size=384,**kwargs)   # img_size=384
     model.default_cfg = _cfg()
     if pretrained:
         # If running Kvasir-SEG, use the first line of code below; If running ISIC-2018 challenge and CVC-ClinicDB, use the second line of code.
-        ckpt = torch.load('pretrained/deit_base_patch16_224-b5f2ef4d.pth')     # 1
-        #ckpt = torch.load('pretrained/deit_base_patch16_384-8de9b5d1.pth')    # 2
+        #ckpt = torch.load('pretrained/deit_base_patch16_224-b5f2ef4d.pth')     # 1
+        ckpt = torch.load('pretrained/deit_base_patch16_384-8de9b5d1.pth')    # 2
         model.load_state_dict(ckpt['model'], strict=False)
     
     pe = model.pos_embed[:, 1:, :].detach()
     pe = pe.transpose(-1, -2)
     pe = pe.view(pe.shape[0], pe.shape[1], int(np.sqrt(pe.shape[2])), int(np.sqrt(pe.shape[2])))
-    pe = F.interpolate(pe, size=(22,22), mode='bilinear', align_corners=True)  # Notice: 352/16 = 22, 384/16 = 24
+    pe = F.interpolate(pe, size=(24,24), mode='bilinear', align_corners=True)  # Notice: 352/16 = 22, 384/16 = 24
     pe = pe.flatten(2)
     pe = pe.transpose(-1, -2)
     model.pos_embed = nn.Parameter(pe)
